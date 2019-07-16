9d6394e8d6d01fe6008eedac029c83df51f12912
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-16 16:03:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -32,7 +32,8 @@ def retrieve_element_from_index(array, index_number)
 end
 
 def retrieve_first_element_from_array(array)
-  words =  ["help", "please", "am","hair"]
+  words =  ["wow", "please", "am","hair"]
+  words[0]
 end
 
 def retrieve_last_element_from_array(array)
