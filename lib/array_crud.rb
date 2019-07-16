2824ad4d89399d39e7ad4fb7e0ea19d19475b582
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-16 15:57:51 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ end
 
 def add_element_to_start_of_array(array, element)
   array = ["dog","cat","fish","frog"]
-  array << "arrays!"
+  array.unshift 
 end
 
 def remove_element_from_end_of_array(array)
