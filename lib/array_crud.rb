aa6a64d3d182ed1e6aefc30a8043960f12beeef0
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-16 15:56:30 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,9 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-famous_cats = ["lil' bub", "grumpy cat", "Maru"]
+array = ["dog","cat","fish","frog"]
  
-famous_cats << "arrays!"
+array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
