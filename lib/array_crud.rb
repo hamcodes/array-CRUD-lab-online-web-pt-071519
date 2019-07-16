f9fc528ffd01385439ea19c770bb4030cc8b7df0
Jeremy Hamilton <jeremyhamilton035@gmail.com>
2019-07-16 15:55:12 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,7 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
 famous_cats = ["lil' bub", "grumpy cat", "Maru"]
  
-famous_cats << "nala cat"
+famous_cats << "array"
 end
 
 def add_element_to_start_of_array(array, element)
